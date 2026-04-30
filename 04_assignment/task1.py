be6dfde58e0358b533f2a9b1ea9dc6f7a8bff404
pinkypeach <elagina_daria@gmx.de>
2026-04-30 20:57:39 +0200

    task1 d)

--- 04_assignment/task1.py
+++ 04_assignment/task1.py
@@ -121,8 +121,26 @@ def d_contraction(A, B, C,
     
     for k_i in range(k):
         # GEMM: xyzl (merge y & l)
-        
+        a_tile =ct.load(A, 
+                        index=(pid_e, pid_a, pid_b, k_i, 0, 0, 0), 
+                        shape =(1,1,1,1,l,x,y))
+        b_tile = ct.load(B,
+                         index=(pid_e, pid_c, k_i, 0,0,0),
+                         shape=(1,1,1,l,y,z))
+        a_tile = ct.permute(a_tile, (0,1,2,3,5,4,6)) #e,a,b,k,x,l,y
 
+        a_tile = ct.reshape(a_tile,(x, l*y))
+        b_tile = ct.reshape(b_tile,(l*y, z))
+
+        acc = ct.mma(a_tile, b_tile, acc)
+    out = ct.reshape(acc, (1,1,1,1,x,z)).astype(ct.float16)
+    ct.store(C, index=(pid_e, pid_a, pid_b, pid_c, 0,0), tile=out)
+
+# eabklxy, ecklyz -> eabcxz
+# GEMM dims: exyz, meaning that you perform a 3D ct.mma inside the kernel. 
+# Sequentialize all other K-dimensions, parallelize the remaining dimensions.
+def e_contraction():
+    print("bla")
 
 def run_kernels():
     e, a, b, c = 16, 8, 32, 64
@@ -155,7 +173,7 @@ def run_kernels():
                     e,a,b,c,k,l,x,y,z))             #dimensions
     
     ref = torch.einsum('eabklxy,ecklyz->eabcxz', a_input.float(), b_input.float()).half()
-    print("Verification:", torch.allclose(c_output, ref, atol=1e-2, rtol=1e-2))
+    print("b) Verification:", torch.allclose(c_output, ref, atol=1e-2, rtol=1e-2))
 
 # Task 1 c)
     c_output = c_output.zero_() #start with empty ouput
@@ -169,7 +187,22 @@ def run_kernels():
                     e,a,b,c,k,l,x,y,z))            
     
     #ref = torch.einsum('eabklxy,ecklyz->eabcxz', a_input.float(), b_input.float()).half()
-    print("Verification:", torch.allclose(c_output, ref, atol=1e-2, rtol=1e-2))
+    print("c) Verification:", torch.allclose(c_output, ref, atol=1e-2, rtol=1e-2))
+
+
+# Task 1 d)
+    c_output = c_output.zero_() #start with empty ouput
+
+    grid_d = (e*a*c*b, )
+
+    ct.launch(torch.cuda.current_stream(),
+                    grid_d,
+                    d_contraction,
+                    (a_input, b_input, c_output,   
+                    e,a,b,c,k,l,x,y,z))            
+    
+    #ref = torch.einsum('eabklxy,ecklyz->eabcxz', a_input.float(), b_input.float()).half()
+    print("d) Verification:", torch.allclose(c_output, ref, atol=1e-2, rtol=1e-2))
 
 
 
